ab023de0b7dee87da444b6df64ed93049eb9b7d9
Prajwol Shrestha <prajwol.shrestha1@outlook.com>
2017-08-10 19:30:18 -0500

    Fourth Commit - Stle sheet

--- PioneerTech.WebApp/UI/Home.aspx.cs
+++ PioneerTech.WebApp/UI/Home.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace PioneerTech.WebApp.UI
 {
-    public partial class PioneerTech : System.Web.UI.MasterPage
+    public partial class Home : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
